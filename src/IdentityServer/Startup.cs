0bbb2ee2e537bcc0855d941251b469c5c2d63b43
zw <wei.zheng>
2019-11-22 14:43:58 +0800

    注释初始化数据库

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -107,7 +107,7 @@ namespace IdentityServer
 
         public void Configure(IApplicationBuilder app)
         {
-            InitializeDatabase(app);
+            //InitializeDatabase(app);
 
             if (Environment.IsDevelopment())
             {
